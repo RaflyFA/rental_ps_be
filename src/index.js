c17bf62fc65ccc7d8f5aa0f8b6369350b022f008
Fauzn Ahmad <fauzanahmad0509@gmail.com>
2025-11-04 20:17:11 +0700

    hlo tot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
-
+import mainRouter from './route/mainpage.js'
 
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/',  mainRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/route/mainpage.js
@@ -0,0 +1,13 @@
+// routes/index.js
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Halo Tot! Ini halaman utama 🚀');
+});
+
+router.get('/about', (req, res) => {
+  res.send('Ini halaman About (pakai type: module).');
+});
+
+export default router;
