7d24076f5aa61b7aa3468c6fe1bc796334b8d2a4
AmmanR <ammanabdullah7@gmail.com>
2025-11-10 22:09:59 +0700

    termul

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mainRouter from './route/mainpage.js'
-console.log("SOEHARTO PAHLAWAN NASIONAL");
+console.log("OJAN ANAK ILANG");
 const app = express();
 const port = 3000
 
